430efd5df25ec3a45b1eaca7d38a99c5f7c1e7e5
Politofr09 <deshidratatorpa@gmail.com>
2025-03-29 13:50:52 +0100

    Major changes: now you have config.ini you can configure the ground station, the tle url and the duration of the orbit predicition
    The ground station appears in the 3d representation.

--- tracker.go
+++ tracker.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"fmt"
 	"sort"
+	"strconv"
 	"strings"
 	"time"
 
@@ -16,7 +17,7 @@ import (
 	"gopkg.in/ini.v1"
 )
 
-func selectSatellite(selectedSatellite string, satellitesList map[string][2]string, scale float64, orbitPath []rl.Vector3, orbitPoints int) (satellite.Satellite, error) {
+func selectSatellite(selectedSatellite string, satellitesList map[string][2]string, scale float64, t time.Time, orbitPath []rl.Vector3, orbitPoints int) (satellite.Satellite, error) {
 	if _, exists := satellitesList[selectedSatellite]; !exists {
 		return satellite.Satellite{}, errors.New("Can't find satellite " + selectedSatellite)
 	}
@@ -26,7 +27,7 @@ func selectSatellite(selectedSatellite string, satellitesList map[string][2]stri
 
 	// Parse the TLE data into a Satellite object
 	sat := satellite.TLEToSat(tleLine1, tleLine2, satellite.GravityWGS84)
-	computeOrbitPath(sat, scale, orbitPath[:], orbitPoints)
+	computeOrbitPath(sat, t, scale, orbitPath[:], orbitPoints)
 	rl.SetWindowTitle("Tracking " + selectedSatellite)
 	return sat, nil
 }
@@ -34,29 +35,31 @@ func selectSatellite(selectedSatellite string, satellitesList map[string][2]stri
 func getSatellitePosition(sat satellite.Satellite, time time.Time, scale float64) rl.Vector3 {
 	position, _ := satellite.Propagate(sat, time.Year(), int(time.Month()), time.Day(), time.Hour(), time.Minute(), time.Second())
 
+	position = satellite.ECIToECEF(position, satellite.GSTimeFromDate(time.Year(), int(time.Month()), time.Day(), time.Hour(), time.Minute(), time.Second()))
+
 	return rl.NewVector3((float32(position.Y / scale)), (float32(position.Z / scale)), (float32(position.X / scale)))
 }
 
-func computeOrbitPath(sat satellite.Satellite, scale float64, orbitPath []rl.Vector3, orbitPoints int) {
-	now := time.Now().UTC().Add(time.Minute * -10) // Remove 10 minutes
-	for i := 0; i < orbitPoints; i++ {
-		futureTime := now.Add(time.Second * time.Duration(i*60)) // Every 60s
-		position, _ := satellite.Propagate(sat, futureTime.Year(), int(futureTime.Month()), futureTime.Day(), futureTime.Hour(), futureTime.Minute(), futureTime.Second())
+func computeOrbitPath(sat satellite.Satellite, t time.Time, scale float64, orbitPath []rl.Vector3, orbitPoints int) {
+	
+	if orbitPoints <= 10 {
+		t = t.Add(-time.Duration(orbitPoints) * time.Minute / 2) // Remove the half
+	} else {
+		t = t.Add(time.Minute * -10) // Remove 10 minutes
+	}
 
-		// Convert to scaled world space
-		orbitPath[i] = rl.NewVector3(
-			float32(position.Y/scale),
-			float32(position.Z/scale),
-			float32(position.X/scale),
-		)
+	for i := 0; i < orbitPoints; i++ {
+		futureTime := t.Add(time.Second * time.Duration(i*60)) // Every 60s
+		orbitPath[i] = getSatellitePosition(sat, futureTime, scale)
 	}
 }
+
 
 func main() {
 	// Load config
 	cfg, err := ini.Load("res/config.ini")
 	if err != nil {
-		fmt.Println("Error reading config file: %v", err)
+		fmt.Printf("Error reading config file: %v\n", err)
 		return
 	}
 
@@ -77,22 +80,45 @@ func main() {
 	}
 
 	rl.SetConfigFlags(rl.FlagWindowResizable | rl.FlagMsaa4xHint)
+	rl.SetTraceLogLevel(rl.LogNone)
 	rl.InitWindow(1280, 720, "No satellite selected yet")
 	rl.MaximizeWindow()
 	defer rl.CloseWindow()
 
-	// selectedSatellite := "NOAA19"
 	selectedSatellite := cfg.Section("Tracker").Key("satellite").String()
-	fmt.Println("Selected satellite: ", selectedSatellite)
 
-	inputText := ""
-	const orbitPoints = 110
-	var orbitPath [orbitPoints]rl.Vector3
-	sat, err := selectSatellite(selectedSatellite, satellites, scale, orbitPath[:], orbitPoints)
-	if err != nil {
-		fmt.Println("Problem selecting satellite: %v", err)
+	// Parse lat/long from config
+	parts := strings.Split(cfg.Section("Tracker").Key("ground_station").String(), ", ")
+	if len(parts) < 2 {
+		fmt.Printf("invalid ground station format")
+		return
 	}
 
+	groundLat, err := strconv.ParseFloat(strings.TrimSpace(parts[0]), 64)
+	if err != nil {
+		fmt.Printf("invalid latitude: %v", err)
+		return
+	}
+
+	groundLon, err := strconv.ParseFloat(strings.TrimSpace(parts[1]), 64)
+	if err != nil {
+		fmt.Printf("invalid longitude: %v", err)
+		return
+	}
+	fmt.Println("[Config] Ground station", groundLon, groundLat)
+
+	now := time.Now().UTC()
+
+	inputText := ""
+	orbitPoints := cfg.Section("Tracker").Key("orbit_duration").MustInt(100)
+
+	fmt.Println("[Config] Orbit points", orbitPoints)
+
+	var orbitPath = make([]rl.Vector3, orbitPoints)
+	sat, err := selectSatellite(selectedSatellite, satellites, scale, now, orbitPath[:], orbitPoints)
+	if err != nil {
+		fmt.Printf("Problem selecting satellite: %q\n", err)
+	}
 
 	// Load shaders
 	crtShader := rl.LoadShader("res/crt.vs", "res/crt.fs")
@@ -105,6 +131,9 @@ func main() {
 	satellite_model := rl.LoadModel("res/satellite.glb")
 	defer rl.UnloadModel(satellite_model)
 
+	bilboardTexture := rl.LoadTexture("res/location-bilboard-map.png")
+	defer rl.UnloadTexture(bilboardTexture)
+	
 	renderTarget := rl.LoadRenderTexture(int32(rl.GetScreenWidth()), int32(rl.GetScreenHeight()))
 	camera := rl.Camera{}
 	camera.Position = rl.NewVector3(-10.0, 8.0, -10.0)
@@ -115,15 +144,20 @@ func main() {
 
 	rl.SetTargetFPS(60)
 
+	// now := time.Now().UTC()
 	for !rl.WindowShouldClose() {
+		// now = now.Add(time.Second * 10)
+		now = time.Now().UTC()
+
+		computeOrbitPath(sat, now, scale, orbitPath[:], orbitPoints)
+		
+
 		// Recreate the render texture if the window is resized
 		if rl.IsWindowResized() {
-			rl.UnloadRenderTexture(renderTarget);
+			rl.UnloadRenderTexture(renderTarget)
 			renderTarget = rl.LoadRenderTexture(int32(rl.GetScreenWidth()), int32(rl.GetScreenHeight()))
-
 		}
 
-		now := time.Now().UTC()
 		rl.SetShaderValue(crtShader, timeLoc, []float32{float32(rl.GetTime())}, rl.ShaderUniformFloat)
 		rl.SetShaderValue(crtShader, resLoc, []float32{float32(rl.GetScreenWidth()), float32(rl.GetScreenHeight())}, rl.ShaderUniformVec2)
 
@@ -153,15 +187,9 @@ func main() {
 
 		rl.BeginMode3D(camera)
 
-		rl.DrawGrid(50, 6378/1000.0)
+		rl.DrawGrid(50, 10)
 
-		// Calculate how many seconds have passed in the day
-		secondsInDay := float32(now.Hour()*3600 + now.Minute()*60 + now.Second())
-
-		// Rotation angle per second = 360 degrees / 86400 seconds = 0.004167 degrees/second
-		rotationAngle := secondsInDay * 360.0 / 86400.0
-
-		rl.DrawModelEx(earth_model, rl.NewVector3(0, 0, 0), rl.NewVector3(0, 1, 0), rotationAngle-5, rl.NewVector3(0.01, 0.01, 0.01), rl.White)
+		rl.DrawModelEx(earth_model, rl.NewVector3(0, 0, 0), rl.NewVector3(0, 1, 0), 180, rl.NewVector3(0.01, 0.01, 0.01), rl.White)
 
 		// Draw orbit
 		for i := 0; i < orbitPoints-1; i++ {
@@ -171,6 +199,15 @@ func main() {
 		rl.DrawModelEx(satellite_model, satPos, rl.NewVector3(0, 0, 0), 0.0, rl.NewVector3(0.0001, 0.0001, 0.0001), rl.White)
 		rl.DrawLine3D(satPos, rl.NewVector3(0, 0, 0), rl.Blue)
 
+		// Draw ground station
+		groundStationECI := satellite.LLAToECI(satellite.LatLong{Latitude: groundLat * satellite.DEG2RAD, Longitude: groundLon * satellite.DEG2RAD}, 0.0, satellite.JDay(now.Year(), int(now.Month()), now.Day(), now.Hour(), now.Minute(), now.Second()))
+		groundStation := satellite.ECIToECEF(groundStationECI, satellite.GSTimeFromDate(now.Year(), int(now.Month()), now.Day(), now.Hour(), now.Minute(), now.Second()))
+
+		rotationMatrix := rl.MatrixRotateY(rl.Deg2rad * 90)
+		groundStationRotated := rl.Vector3Transform(rl.NewVector3(float32(groundStation.X), float32(groundStation.Z), -float32(groundStation.Y)), rotationMatrix)
+		// rl.DrawSphere(rl.NewVector3(groundStationRotated.X/1300, groundStationRotated.Y/1300, groundStationRotated.Z/1300), 0.1, rl.White)
+		rl.DrawBillboard(camera, bilboardTexture, rl.NewVector3(groundStationRotated.X/1260, groundStationRotated.Y/1260+0.1, groundStationRotated.Z/1260), 0.5, rl.White)
+
 		rl.EndMode3D()
 
 		rl.EndTextureMode()
@@ -179,14 +216,13 @@ func main() {
 		rl.BeginShaderMode(crtShader)
 
 		rl.DrawTextureRec(
-			renderTarget.Texture, 
+			renderTarget.Texture,
 			rl.NewRectangle(0, 0, float32(rl.GetScreenWidth()), -float32(rl.GetScreenHeight())), // Flip vertically
 			rl.NewVector2(0, 0), // Adjust position accordingly
 			rl.White,
 		)
 		rl.EndShaderMode()
 
-		
 		// Render the UI
 		date_text := now.String()
 		rl.DrawText(date_text, 10, 10, 20, rl.Yellow)
@@ -198,7 +234,7 @@ func main() {
 		inputText = strings.ToUpper(inputText)
 		if inputText != selectedSatellite {
 			selectedSatellite = inputText
-			tempSat, err := selectSatellite(selectedSatellite, satellites, scale, orbitPath[:], orbitPoints)
+			tempSat, err := selectSatellite(selectedSatellite, satellites, scale, now, orbitPath[:], orbitPoints)
 			if err == nil {
 				sat = tempSat
 			} // Don't need to handle the error here
